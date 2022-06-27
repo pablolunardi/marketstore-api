cfd6699bad3eab4f89c4e7db525b38b07d44b846
Pablo Augusto Lunardi <pablolunardi@gmail.com>
2022-06-26 22:11:50 -0300

    Added in memory auth for Sring Security

--- src/main/java/com/lunardi/marketstore/core/security/WebSecurityConfig.java
+++ src/main/java/com/lunardi/marketstore/core/security/WebSecurityConfig.java
@@ -1,15 +1,41 @@
 package com.lunardi.marketstore.core.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/**").permitAll();
+		http.httpBasic()
+				.and()
+				.authorizeRequests()
+					.anyRequest().authenticated()
+				.and()
+					.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+					.csrf().disable();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("test")
+				.password(passwordEncoder().encode("test"))
+				.roles("ADMIN");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
