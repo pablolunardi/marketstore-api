1d67ebeff9ea898cc173f6f57f5f7538a1b90d59
Pablo Augusto Lunardi <pablolunardi@gmail.com>
2022-07-03 23:16:13 -0300

    Set up for authorization server

--- src/main/java/com/lunardi/marketstore/core/security/WebSecurityConfig.java
+++ src/main/java/com/lunardi/marketstore/core/security/WebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
